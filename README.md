1d84d57c9947fb84c23ac12484ef0e25202d2f50
802ScubaFish <christianrdow@gmail.com>
2020-08-25 16:42:30 -0400

    multiple functionalities working, commiting as a working stopping point to fall back to just incase

--- README.md
+++ README.md
@@ -13,3 +13,14 @@ File Structure:
 bg = backgrounds / buttons / icons
 pet = individual pet states
 sprite.css = all styling for the pet
+
+Code Style:
+
+I have set up prettier to auto format this project. 
+The scripts are set up so that if there were any team members they could run 'npm run format' to auto format their own documents accordingly.
+
+'npm run format:check' iterates over the javascript formatting similar to above.
+
+Testing:
+
+I've installed and added Jest to the scripts for testing purposes.
---
+++ src/gameState.js
@@ -0,0 +1,151 @@
+import { modFox, modScene } from "./ui";
+import { RAIN_CHANCE, SCENES, DAY_LENGTH, NIGHT_LENGTH, getNextDieTime, getNextHungerTime, getNextPoopTime } from "./constants";
+
+const gameState = {
+  current: "INIT",
+  clock: 1,
+  wakeTime: -1,
+  sleepTime: -1,
+  hungryTime: -1,
+  dieTime: -1,
+  timeToStartCelebrating: -1,
+  timeToEndCelerating: -1,
+
+  tick() {
+    this.clock++;
+    console.log("clock", this.clock);
+
+    if (this.clock === this.wakeTime) {
+      this.wake();
+    }
+    else if (this.clock === this.sleepTime) {
+      this.sleep();
+    }
+    else if (this.clock === this.hungryTime) {
+      this.getHungry();
+    }
+    else if (this.clock === this.dieTime) {
+      this.die();
+    }
+    else if (this.clock === this.timeToStartCelebrating) {
+      this.startCelebrating();
+    }
+    else if (this.clock === this.timeToEndCelerating) {
+      this.endCelebrating();
+    }
+
+    return this.clock;
+  },
+
+  startGame() {
+    
+    this.current = "HATCHING";
+    this.wakeTime = this.clock + 3;
+    modFox('egg');
+    modScene('day');
+  },
+
+  wake() {
+  
+    this.current = "IDLING";
+    this.wakeTime = -1;
+    this.scene = Math.random() > RAIN_CHANCE ? 0 : 1;
+    modScene(SCENES[this.scene]);
+    this.sleepTime = this.clock + DAY_LENGTH;
+    this.hungryTime = getNextHungerTime(this.clock);
+    this.determineFoxState();
+  },
+
+  sleep() {
+    this.state = "SLEEP";
+    modFox('sleep');
+    modScene('night');
+    this.wakeTime = this.clock + NIGHT_LENGTH;
+  },
+
+  getHungry() {
+    this.current = 'HUNGRY';
+    this.dieTime = getNextDieTime(this.clock);
+    this.hungryTime = -1;
+    modFox('hungry');
+  },
+
+  die() {
+    console.log('fox has died')
+  },
+
+  startCelebrating() {
+    this.current = "CELBRATING";
+    modFox('celebrate');
+    this.timeToStartCelebrating = -1;
+    this.timeToEndCelerating = this.clock + 2;
+  },
+
+  endCelebrating() {
+    this.timeToEndCelerating = -1;
+    this.current = 'IDLING';
+    this.determineFoxState();
+
+  },
+
+  determineFoxState() {
+    if (this.current === 'IDLING') {
+      if(SCENES[this.scene] === 'rain') {
+        modFox('rain')
+      }
+      else {
+        modFox('idling')
+      }
+    }
+  },
+
+  handleUserAction(icon) {
+    if (
+      ["SLEEP", "FEEDING", "CELEBRATING", "HATCHING"].includes(this.current)
+    ) {
+      // Do Nothing
+      return;
+    }
+    if (this.current === "INIT" || this.current === "DEAD") {
+      this.startGame();
+      return;
+    }
+
+    switch (icon) {
+      case "weather":
+        this.changeWeather();
+        break;
+      case "poop":
+        this.cleanUpPoop();
+        break;
+      case "fish":
+        this.feed();
+        break;
+    }
+  },
+
+  changeWeather() {
+    console.log('change weather');
+  },
+
+  cleanUpPoop() {
+    console.log('clean up poop');
+  },
+
+  feed() {
+
+  if (this.current !== 'HUNGRY') {
+    return
+  }
+
+  this.current = 'FEEDING';
+  this.dieTime = -1;
+  this.poopTime = getNextPoopTime(this.clock);
+  modFox('eating');
+  this.timeToStartCelebrating = this.clock + 2;
+  }
+
+};
+
+export const handleUserAction = gameState.handleUserAction.bind(gameState);
+export default gameState;
