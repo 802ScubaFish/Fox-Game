e38cdc2e7ca4cbb39084fa00cd8ce451666d9b05
802ScubaFish <christianrdow@gmail.com>
2020-08-26 20:31:37 -0400

    correctly building using parcel

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const port = process.env.PORT || 5000;
-const public = path.resolve('./src');
+const public = path.resolve('./dist');
 
 app.use(express.static(public));
 
@@ -11,6 +11,5 @@ app.get('*', (req, res) => {
 })
 
 app.listen(port, () => console.log(`Server running on port ${port}!`))
-
 
 // This page is just setting up a basic express server - - - - - - - - - - - - -
